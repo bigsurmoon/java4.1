975a51b916a36c4cecc739fad4516b0bee893aeb
bigsurmoon <vinny4000@gmail.com>
2021-09-27 17:02:17 +0700

    init commit

---
+++ src/BonusMilesServices.java
@@ -0,0 +1,11 @@
+public class BonusMilesServices {
+    public int calculate(int price) {
+
+        int moneyForMile = 20;
+        int bonusMiles = price / moneyForMile;
+
+        return bonusMiles;
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BonusMilesServices service = new BonusMilesServices();
+        int price = 10_000;
+        int miles = service.calculate(32_000);
+        System.out.println(miles);
+
+    }
+}
+
