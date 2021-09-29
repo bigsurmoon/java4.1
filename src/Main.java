830487ae6aa17acfb9e95da5744b9f8e3a11d4c2
Ed <85837973+bigsurmoon@users.noreply.github.com>
2021-09-29 16:16:24 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         BonusMilesServices service = new BonusMilesServices();
-        int price = 10_000;
         int miles = service.calculate(32_000);
         System.out.println(miles);
     }
